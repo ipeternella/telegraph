518356651d975c6ba165a73fc3e7655c5ba236c9
IgooorGP <igor.feq@gmail.com>
2020-11-27 23:40:24 -0300

    improve chatroom integration test.

--- tests/api/v1/test_chatroom.py
+++ tests/api/v1/test_chatroom.py
@@ -12,21 +12,32 @@ from src.settings import SERVER_FULL_BIND_ADDRESS
 from src.startup import fastapi_server
 
 
-class TestSomething(test.TestCase):
-    async def test_something_async(self):
-        """
-        Should get all chatrooms.
-        """
+class ChatroomApiV1Tests(test.TestCase):
+    """
+    Chatroom-related v1 api integration tests.
+    """
+
+    async def test_should_create_and_get_all_chatrooms(self):
         # arrange
         endpoint = "api/v1/chatroom"
-        chatroom_creation_data = {"name": f"test room {uuid.uuid4()}", "description": "hehe", "max_concurrent_users": 2}
+        chatroom_creation_data = {
+            "name": f"test room {uuid.uuid4()}",
+            "description": "something",
+            "max_concurrent_users": 2,
+        }
 
         # act
         async with AsyncClient(app=fastapi_server, base_url=SERVER_FULL_BIND_ADDRESS) as client:
-            response1 = await client.post(endpoint, data=json.dumps(chatroom_creation_data))
+            creation_response = await client.post(endpoint, data=json.dumps(chatroom_creation_data))
 
-        chatrooms = await ChatRoom.all()
+        all_db_chatrooms = await ChatRoom.all()
+        creation_response_body = creation_response.json()
 
-        # assert
-        assert response1.status_code == 200
-        assert len(chatrooms) == 1
+        # assert -- http response
+        assert creation_response.status_code == 200
+        assert creation_response_body["name"] == chatroom_creation_data["name"]
+        assert creation_response_body["description"] == chatroom_creation_data["description"]
+        assert creation_response_body["max_concurrent_users"] == chatroom_creation_data["max_concurrent_users"]
+
+        # assert - db state
+        assert len(all_db_chatrooms) == 1
