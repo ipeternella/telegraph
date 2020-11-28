a43fe28031ab48fd8c1c49cfc1eb29fc740f4336
IgooorGP <igor.feq@gmail.com>
2020-11-28 18:31:25 -0300

    add live reloading server config

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ Entrypoint of the application which starts the server.
 """
 import uvicorn
 
+from src.settings import DEBUG
 from src.settings import SERVER_BIND_ADDRESS
 from src.settings import SERVER_BIND_PORT
-from src.startup import fastapi_server
 
 if __name__ == "__main__":
-    uvicorn.run(fastapi_server, host=SERVER_BIND_ADDRESS, port=SERVER_BIND_PORT)
+    uvicorn.run(app="src.startup:fastapi_server", host=SERVER_BIND_ADDRESS, port=SERVER_BIND_PORT, reload=DEBUG)
